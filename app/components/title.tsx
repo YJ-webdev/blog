1da02338836eb1243293d8a42382f43f0385b674
Youjung Lee <youjung.dev@gmail.com>
2025-04-04 09:27:15 +0900

    adding url

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -16,11 +16,14 @@ export const Title = () => {
     <div className="mt-3 px-2 bg-white dark:bg-[#1f1f1f] max-w-[1000px] mx-auto w-full">
       <h1 className="font-bold md:text-6xl text-5xl">
         {pathname === '/my-posts' && '내 포스트'}
-        {pathname === '/edit' && ''}
         {pathname === '/' && '레인지 저널'}
-        {pathname.includes('/new-post') && ''}
         {pathname.includes('/tag') && slug && formatSlug(slug)}
-        {pathname === '/not-found' && '레인지 저널'}
+        {!['/my-posts', '/edit', '/'].includes(pathname) &&
+          !pathname.includes('/new-post') &&
+          !pathname.includes('/tag') &&
+          !pathname.includes('/edit') &&
+          !pathname.includes('/post') &&
+          '레인지 저널'}
       </h1>
     </div>
   );
