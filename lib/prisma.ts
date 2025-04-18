62ce5ad9902591baf77ebdf6d7a74ef0d2647b87
Youjung Lee <youjung.dev@gmail.com>
2025-04-18 22:44:00 +0900

    debug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,18 +2,11 @@
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-export const prisma = new PrismaClient({
-  omit: {
-    post: {
-      updatedAt: true,
-    },
-  },
-}).$extends(withAccelerate());
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<PrismaClient['$extends']>;
+};
 
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-// //Using this approach ensures that only one instance of Prisma Client exists, even during hot-reloading in development.
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
